999abbec324cc64407c041b7db27b83fc35fd1ec
isaya <ionthefirst97@gmail.com>
2023-12-10 11:53:40 +0300

    Container with morphism decoration

---
+++ lib/homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey.shade300,
+        body: Center(
+          child: Container(
+            height: 250,
+            width: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade500,
+                    offset: const Offset(4.0, 4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0),
+                const BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1),
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:widget_of_the_day/homepage.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ));
