65937300e55a00686a98fceadd51198f830b19e4
isaya <ionthefirst97@gmail.com>
2023-12-10 13:02:15 +0300

    Created list builder

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,23 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:widget_of_the_day/square.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  final List post = ['Isaya', 'Osward', 'flutter', 'is','Awesome'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: const [
-          Mysquare(),
-          Mysquare(),
-          Mysquare(),
-          Mysquare(),
-          Mysquare(),
-          Mysquare(),
-          Mysquare(),
-          Mysquare(),
-        ],
-      ),
+      body: ListView.builder(
+          itemCount: post.length,
+          itemBuilder: (context, index) {
+            return Mysquare(
+              child: post[index],
+            );
+          }),
     );
   }
 }
--- lib/square.dart
+++ lib/square.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Mysquare extends StatelessWidget {
-  const Mysquare({super.key});
+  final String child;
+  Mysquare({required this.child});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 200,
-              color: Colors.deepPurple.shade200,
-            ),
-          );
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 200,
+        color: Colors.deepPurple.shade200,
+        child: Center(
+          child: Text(
+            child,
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+    );
   }
 }
